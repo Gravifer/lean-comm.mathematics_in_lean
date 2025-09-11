847cc612dde2f685cd032eb212acfc716ed06e6b
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-09-11 17:35:28 +0800

    read(mil): finish §3.3

--- MIL/C03_Logic/S03_Negation.lean
+++ MIL/C03_Logic/S03_Negation.lean
@@ -8,8 +8,8 @@ variable (a b : ℝ)
 
 example (h : a < b) : ¬b < a := by
   intro h'
-  have : a < a := lt_trans h h'
-  apply lt_irrefl a this
+  have : a < a := lt_trans h h'  -- when you use `have` without providing a label,
+  apply lt_irrefl a this  -- Lean uses the name `this`, providing a convenient way to refer back to it.
 
 def FnUb (f : ℝ → ℝ) (a : ℝ) : Prop :=
   ∀ x, f x ≤ a
@@ -32,11 +32,16 @@ example (h : ∀ a, ∃ x, f x > a) : ¬FnHasUb f := by
   have : f x ≤ a := fnuba x
   linarith
 
-example (h : ∀ a, ∃ x, f x < a) : ¬FnHasLb f :=
-  sorry
+example (h : ∀ a, ∃ x, f x < a) : ¬FnHasLb f := by
+  intro fnlb;  rcases fnlb with ⟨a, fnlba⟩
+  rcases h a with ⟨x, hx⟩
+  have : a ≤ f x := fnlba x
+  linarith
 
-example : ¬FnHasUb fun x ↦ x :=
-  sorry
+example : ¬FnHasUb fun x ↦ x := by
+  intro fnub;  rcases fnub with ⟨a, fnuba⟩
+  have : a + 1 ≤ a := fnuba (a + 1)
+  linarith
 
 #check (not_le_of_gt : a > b → ¬a ≤ b)
 #check (not_lt_of_ge : a ≥ b → ¬a < b)
@@ -44,20 +49,31 @@ example : ¬FnHasUb fun x ↦ x :=
 #check (le_of_not_gt : ¬a > b → a ≤ b)
 
 example (h : Monotone f) (h' : f a < f b) : a < b := by
-  sorry
+  -- unfold Monotone at h
+  have : ¬a ≥ b := by
+    intro h''
+    have : f a ≥ f b := h h''
+    linarith
+  linarith
 
 example (h : a ≤ b) (h' : f b < f a) : ¬Monotone f := by
-  sorry
+  intro monof
+  have : f a ≤ f b := monof h
+  linarith
 
 example : ¬∀ {f : ℝ → ℝ}, Monotone f → ∀ {a b}, f a ≤ f b → a ≤ b := by
   intro h
   let f := fun x : ℝ ↦ (0 : ℝ)
-  have monof : Monotone f := by sorry
+  have monof : Monotone f := monotone_const
   have h' : f 1 ≤ f 0 := le_refl _
-  sorry
+  have : (1 : ℝ) ≤ 0 := h monof h'
+  linarith
 
 example (x : ℝ) (h : ∀ ε > 0, x < ε) : x ≤ 0 := by
-  sorry
+  apply le_of_not_gt;  intro xpos
+  have : x / 2 > 0 := by linarith
+  have : x < x / 2 := h (x / 2) this
+  linarith
 
 end
 
@@ -65,16 +81,23 @@ section
 variable {α : Type*} (P : α → Prop) (Q : Prop)
 
 example (h : ¬∃ x, P x) : ∀ x, ¬P x := by
-  sorry
+  intro x px
+  apply h
+  use x
 
 example (h : ∀ x, ¬P x) : ¬∃ x, P x := by
-  sorry
+  intro epx
+  rcases epx with ⟨x, px⟩
+  exact h x px
 
-example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
-  sorry
+example (h : ¬∀ x, P x) : ∃ x, ¬P x := by -- EM
+  classical push_neg at h -- double neg elimination, which is classical
+  exact h
 
 example (h : ∃ x, ¬P x) : ¬∀ x, P x := by
-  sorry
+  rcases h with ⟨x, npx⟩
+  intro apx
+  exact npx (apx x)
 
 example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
   by_contra h'
@@ -85,10 +108,12 @@ example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
   exact h' ⟨x, h''⟩
 
 example (h : ¬¬Q) : Q := by
-  sorry
+  push_neg at h
+  assumption
 
 example (h : Q) : ¬¬Q := by
-  sorry
+  push_neg
+  exact h
 
 end
 
@@ -96,7 +121,10 @@ section
 variable (f : ℝ → ℝ)
 
 example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
-  sorry
+  unfold FnHasUb FnUb at h
+  push_neg at h
+  intro a
+  exact h a
 
 example (h : ¬∀ a, ∃ x, f x > a) : FnHasUb f := by
   push_neg at h
@@ -108,7 +136,9 @@ example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
   exact h
 
 example (h : ¬Monotone f) : ∃ x y, x ≤ y ∧ f y < f x := by
-  sorry
+  unfold Monotone at h
+  push_neg at h
+  exact h
 
 example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
   contrapose! h
@@ -136,4 +166,3 @@ example (h : 0 < 0) : a > 37 := by
   contradiction
 
 end
-
