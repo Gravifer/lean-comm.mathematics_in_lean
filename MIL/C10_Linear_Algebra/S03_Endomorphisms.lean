a051d6f2613add75ac05166959429efef9e7dede
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-09-24 00:26:09 +0800

    read(mil): finish §10.3

--- MIL/C10_Linear_Algebra/S03_Endomorphisms.lean
+++ MIL/C10_Linear_Algebra/S03_Endomorphisms.lean
@@ -32,16 +32,43 @@ example (φ : End K V) : aeval φ (X : K[X]) = φ :=
 #check LinearMap.mem_ker
 
 example (P Q : K[X]) (h : IsCoprime P Q) (φ : End K V) : ker (aeval φ P) ⊓ ker (aeval φ Q) = ⊥ := by
-  sorry
+  rcases h with ⟨P', Q', h⟩
+  rw [Submodule.eq_bot_iff]
+  rintro x hx; rw [Submodule.mem_inf, mem_ker, mem_ker] at hx
+  obtain ⟨hxp, hxq⟩ := hx
+  replace h := congr((aeval φ) $h.symm x)
+  simp [hxp, hxq] at h; assumption
 
 #check Submodule.add_mem_sup
 #check map_mul
 #check End.mul_apply
 #check LinearMap.ker_le_ker_comp
-
+#check Polynomial
 example (P Q : K[X]) (h : IsCoprime P Q) (φ : End K V) :
     ker (aeval φ P) ⊔ ker (aeval φ Q) = ker (aeval φ (P*Q)) := by
-  sorry
+  ext x; simp [mem_ker, Submodule.mem_sup]; constructor <;> intro prec
+  · rcases prec with ⟨y, hy, z, hz, rfl⟩
+    have {φ : End K V} {P Q : K[X]}
+          : (aeval φ) P * (aeval φ) Q = (aeval φ) Q * (aeval φ) P := by
+      rw [← map_mul, ← map_mul, mul_comm]
+    simp [hy, hz]; rw [← mul_apply, this, mul_apply, hy]; simp
+  · rw [<-mul_apply, <-map_mul] at prec
+    rcases h with ⟨P', Q', h⟩
+    let y := aeval φ (Q'*Q) x
+    let z := aeval φ (P'*P) x
+    have key : y + z = x := by
+      subst y z
+      simpa [add_comm] using congr((aeval φ) $h x)
+    use y, by
+      subst y
+      rw [<-mul_apply, <-map_mul, mul_comm Q', <-mul_assoc, mul_comm, map_mul, mul_apply, prec]
+      simp
+    use z, by
+      subst z
+      rw [<-mul_apply, <-map_mul, mul_comm, mul_assoc, map_mul, mul_apply, prec]
+      simp
+
+
 example (φ : End K V) (a : K) : φ.eigenspace a = LinearMap.ker (φ - a • 1) :=
   End.eigenspace_def
 
@@ -68,4 +95,3 @@ example [FiniteDimensional K V] (φ : End K V) (a : K) :
 -- Cayley-Hamilton
 example [FiniteDimensional K V] (φ : End K V) : aeval φ φ.charpoly = 0 :=
   φ.aeval_self_charpoly
-
