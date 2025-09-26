7e35ca060e0895c89e8f43bae5193041d90e9d6e
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-09-26 18:17:34 +0800

    read(mil): finish §11.1

--- MIL/C11_Topology/S01_Filters.lean
+++ MIL/C11_Topology/S01_Filters.lean
@@ -6,23 +6,38 @@ open Set Filter Topology
 def principal {α : Type*} (s : Set α) : Filter α
     where
   sets := { t | s ⊆ t }
-  univ_sets := sorry
-  sets_of_superset := sorry
-  inter_sets := sorry
+  univ_sets := subset_univ s
+  sets_of_superset := Subset.trans
+  inter_sets := subset_inter
 
 example : Filter ℕ :=
   { sets := { s | ∃ a, ∀ b, a ≤ b → b ∈ s }
-    univ_sets := sorry
-    sets_of_superset := sorry
-    inter_sets := sorry }
+    univ_sets := by use 0; simp
+    sets_of_superset := by
+      rintro x y ⟨a, hx⟩ hxy
+      use a; intro b hb
+      exact hxy (hx b hb)
+    inter_sets := by
+      rintro x y ⟨a, hx⟩ ⟨a', hy⟩
+      use max a a'; intro b hb
+      refine ⟨hx b ?_, hy b ?_⟩
+      all_goals simp_all
+  }
+
+variable (x ε : ℝ) in
+#check Ioo (x - ε) (x + ε) -- interval open-open
+variable (x ε : ℝ) in
+#check Ioc (x - ε) (x + ε)
 
 def Tendsto₁ {X Y : Type*} (f : X → Y) (F : Filter X) (G : Filter Y) :=
   ∀ V ∈ G, f ⁻¹' V ∈ F
-
+/- The problem with `Tendsto₁` is that it exposes a quantifier and elements of `G`,
+  and it hides the intuition that we get by viewing filters as generalized sets. -/
 def Tendsto₂ {X Y : Type*} (f : X → Y) (F : Filter X) (G : Filter Y) :=
-  map f F ≤ G
+  map f F ≤ G -- `Filter.map` is the pushforward of `F` along `f`
+#print Tendsto
 
-example {X Y : Type*} (f : X → Y) (F : Filter X) (G : Filter Y) :
+lemma Tendsto₂iffTendsto₁ {X Y : Type*} (f : X → Y) (F : Filter X) (G : Filter Y) :
     Tendsto₂ f F G ↔ Tendsto₁ f F G :=
   Iff.rfl
 
@@ -33,8 +48,11 @@ example {X Y : Type*} (f : X → Y) (F : Filter X) (G : Filter Y) :
     ∀ {α β γ} {f : Filter α} {m : α → β} {m' : β → γ}, map m' (map m f) = map (m' ∘ m) f)
 
 example {X Y Z : Type*} {F : Filter X} {G : Filter Y} {H : Filter Z} {f : X → Y} {g : Y → Z}
-    (hf : Tendsto₁ f F G) (hg : Tendsto₁ g G H) : Tendsto₁ (g ∘ f) F H :=
-  sorry
+    (hf : Tendsto₁ f F G) (hg : Tendsto₁ g G H) : Tendsto₁ (g ∘ f) F H := by
+  -- rw [← Tendsto₂iffTendsto₁, Tendsto₂, ← map_map] at *
+  calc map g (map f F)
+    _ ≤ map g G := map_mono hf
+    _ ≤ H := hg
 
 variable (f : ℝ → ℝ) (x₀ y₀ : ℝ)
 
@@ -54,12 +72,25 @@ example : 𝓝 (x₀, y₀) = 𝓝 x₀ ×ˢ 𝓝 y₀ :=
 
 #check le_inf_iff
 
+#print Filter.instSProd
 example (f : ℕ → ℝ × ℝ) (x₀ y₀ : ℝ) :
     Tendsto f atTop (𝓝 (x₀, y₀)) ↔
-      Tendsto (Prod.fst ∘ f) atTop (𝓝 x₀) ∧ Tendsto (Prod.snd ∘ f) atTop (𝓝 y₀) :=
-  sorry
+      Tendsto (Prod.fst ∘ f) atTop (𝓝 x₀) ∧ Tendsto (Prod.snd ∘ f) atTop (𝓝 y₀) := by
+  rw [nhds_prod_eq]
+  simp only [SProd.sprod]
+  rw [tendsto_inf]
+  unfold Tendsto
+  simp only [← map_le_iff_le_comap, map_map]
 
-example (x₀ : ℝ) : HasBasis (𝓝 x₀) (fun ε : ℝ ↦ 0 < ε) fun ε ↦ Ioo (x₀ - ε) (x₀ + ε) :=
+/- The ordered type `Filter X` is actually a **complete** lattice,
+    by using the trivial filter with `∅` in it as `⊥`.-/
+set_option pp.proofs true in
+example: ∀ α, 𝓟 ∅ = (⊥ : Filter α) := by -- simp -- the trivial filter
+  intro α; unfold Filter.principal; congr
+  show {t | ∅ ⊆ t} = @univ (Set α)
+  simp
+
+example (x₀ : ℝ) : HasBasis (𝓝 x₀) (fun ε : ℝ ↦ 0 < ε) (fun ε ↦ Ioo (x₀ - ε) (x₀ + ε)) :=
   nhds_basis_Ioo_pos x₀
 
 example (u : ℕ → ℝ) (x₀ : ℝ) :
@@ -95,11 +126,17 @@ example (P Q R : ℕ → Prop) (hP : ∀ᶠ n in atTop, P n) (hQ : ∀ᶠ n in a
   filter_upwards [hP, hQ, hR] with n h h' h''
   exact h'' ⟨h, h'⟩
 
+#check Filter.Eventually
+/- the filter `μ.ae` of comeagre sets is not very useful as the source or target of `Tendsto`,
+    but it can be conveniently used with `Eventually` to say that a property holds for almost every point. -/
+
 #check mem_closure_iff_clusterPt
 #check le_principal_iff
 #check neBot_of_le
 
 example (u : ℕ → ℝ) (M : Set ℝ) (x : ℝ) (hux : Tendsto u atTop (𝓝 x))
-    (huM : ∀ᶠ n in atTop, u n ∈ M) : x ∈ closure M :=
-  sorry
-
+    (huM : ∀ᶠ n in atTop, u n ∈ M) : x ∈ closure M := by
+  simp only [mem_closure_iff_clusterPt, ClusterPt]
+  refine neBot_of_le (?_ : map u atTop ≤ (𝓝 x ⊓ 𝓟 M))
+  have: map u atTop ≤ 𝓟 M := le_principal_iff.mpr huM
+  exact le_inf hux this
